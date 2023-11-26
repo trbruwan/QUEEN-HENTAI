1a926334bf52ce98b8b704c3ceb93529951a8bcf
trbruwan <151646682+trbruwan@users.noreply.github.com>
2023-11-26 08:46:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.owner = [
   [''] 
 ] //Number of owners
 
-global.mods = ['94767939688','94767939688'] 
-global.prems = ['94767939688', '94767939688', '94767939688']
+global.mods = ['94767627848','94767627848'] 
+global.prems = ['94767627848', '94767627848', '94767627848']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -49,13 +49,13 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = '💝 Queen Hentai 💝'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
+global.packname = '💝 Queen Hentai 💝 Created By 💝 ×͜×ᵃʳⁱᵃˢ᭄𝒏𝒂𝒑𝒖𝒓𝒂᭄ᵒᶠᶠⁱᶜⁱᵃˡ࿐ 💝' 
+global.author = '💝 ×͜×ᵃʳⁱᵃˢ᭄𝒏𝒂𝒑𝒖𝒓𝒂᭄ᵒᶠᶠⁱᶜⁱᵃˡ࿐ 💝' 
 global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
 global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
 global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
 global.fgyt = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
+global.fgpyp = 'https://chat.whatsapp.com/Ls8WVfsR6jbIe3nihzwe7J'
 global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
 
 
